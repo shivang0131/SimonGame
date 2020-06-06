f17b4bb00c5dee7fab5cfc8ed8193caba930f78e
shivang0131 <43657784+shivang0131@users.noreply.github.com>
2020-06-07 01:00:58 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,86 @@
+var buttonColor = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+$(document).on("keydown", function()
+{
+    if(level === 0){nextSequence();}
+});
+
+
+
+$(".btn").on("click", function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length -1);
+})
+
+
+
+function playSound(name)
+{
+    var audio = new Audio(name + ".mp3");
+    audio.play();
+
+}
+
+function animatePress(currentColor)
+{
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+function nextSequence() {
+
+    level++;
+    $("#level-title").text("Level "+ level);
+	var randomNumber = Math.floor(Math.random()*4);    
+    var randomChosenColor = buttonColor[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+    
+    
+}
+
+function checkAnswer(currentLevel)
+{
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel])
+    {
+        console.log("SUCCESS!!");
+        if(currentLevel == gamePattern.length -1)
+        {
+            setTimeout(nextSequence, 1000);
+            userClickedPattern = [];
+        }
+    }
+    else
+    {
+        console.log("WRONG!!");
+        playSound("wrong");
+        $("#level-title").text("Game Over! Press a key to Restart");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        startOver();
+    }
+}
+
+
+function startOver()
+{
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+}
+
+
+
+
